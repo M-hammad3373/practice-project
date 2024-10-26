5980d21d3220d46c87b3aeb10df7c7db43433c58
M-hammad3373 <alihammadg2003@gmail.com>
2024-10-27 01:26:01 +0500

    CGANGE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
+import HeroSection from '@/components/HeroSection';
+import { Spotlight } from '@/components/ui/Spotlight';
+import FeaturedCourses from '@/components/FeaturedCourses';
+import WhyChooseUs from '@/components/WhyChooseUs';
+import FeaturedWabinars from '@/components/Wabinars';
+import TestiMonialCard from '@/components/TestiMonialCard';
+import Footer from '@/components/Footer';
 import React from 'react'
 
-import HeroSection from '@/components/HeroSection'
-import FeaturedCourses from '@/components/FeaturedCourses'
-import WhyChooseUs from '@/components/WhyChooseUs'
-import TestiMonialCard from '@/components/TestiMonialCard'
-
-import Wabinars from '@/components/Wabinars'
-
-export const page = () => {
-  return (
+const page = () => {
+  return(
     <div>
-  
-      <HeroSection/>
-      <FeaturedCourses/>
-      <WhyChooseUs/>
-      <Wabinars/>
-      <TestiMonialCard/>
-      
+        <HeroSection/>
+        <Spotlight/>
+        <FeaturedCourses/>
+        <WhyChooseUs/>
+        <FeaturedWabinars/>
+        <TestiMonialCard/>
+        <Footer/>
 
     </div>
   )
 }
 
 export default page
-

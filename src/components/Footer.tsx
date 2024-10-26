c64c0672b46ee0252dba6d98a37c2c1c0f80f006
M-hammad3373 <alihammadg2003@gmail.com>
2024-10-27 01:36:21 +0500

    changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function Footer() {
     return (
       <footer className="bg-black text-gray-400 py-12  bottom-0 left-0 w-full">
@@ -13,10 +15,10 @@ function Footer() {
           <div>
             <h3 className="text-lg font-bold text-white mb-4">Links</h3>
             <ul>
-              <li><a href="/" className="hover:text-white">Home</a></li>
-              <li><a href="#" className="hover:text-white">About</a></li>
-              <li><a href="/Courses" className="hover:text-white">Services</a></li>
-              <li><a href="/contact" className="hover:text-white">Contact</a></li>
+              <li><Link href="/" className="hover:text-white">Home</Link></li>
+              <li><Link href="#" className="hover:text-white">About</Link></li>
+              <li><Link href="/Courses" className="hover:text-white">Services</Link></li>
+              <li><Link href="/contact" className="hover:text-white">Contact</Link></li>
             </ul>
           </div>
   
@@ -24,18 +26,18 @@ function Footer() {
           <div>
             <h3 className="text-lg font-bold text-white mb-4">Follow Us</h3>
             <ul>
-            <li><a href="https://www.linkedin.com/in/hammad-ali-a805b228b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="hover:text-white">LinkedIn</a></li>
-              <li><a href="https://github.com/M-hammad3373" className="hover:text-white">Github</a></li>
-              <li><a href="https://vercel.com/new?teamSlug=hammad-alis-projects-b73d4f3a" className="hover:text-white">Portfolio</a></li>
-              <li><a href="https://www.instagram.com/hammad_ali_017?utm_source=qr&igsh=MTRrZHdhdnJndHJpdw==" className="hover:text-white">Instagram</a></li>
-              <li><a href="https://www.facebook.com/hammad.ali.77577?mibextid=ZbWKwL" className="hover:text-white">FaceBook</a></li>
+            <li><Link href="https://www.linkedin.com/in/hammad-ali-a805b228b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="hover:text-white">LinkedIn</Link></li>
+              <li><Link href="https://github.com/M-hammad3373" className="hover:text-white">Github</Link></li>
+              <li><Link href="https://vercel.com/new?teamSlug=hammad-alis-projects-b73d4f3a" className="hover:text-white">Portfolio</Link></li>
+              <li><Link href="https://www.instagram.com/hammad_ali_017?utm_source=qr&igsh=MTRrZHdhdnJndHJpdw==" className="hover:text-white">Instagram</Link></li>
+              <li><Link href="https://www.facebook.com/hammad.ali.77577?mibextid=ZbWKwL" className="hover:text-white">FaceBook</Link></li>
               </ul>
           </div>
   
          
           <div>
             <h3 className="text-lg font-bold text-white mb-4">Contact Us</h3>
-            <p>Email: <a href="mailto:contact@c2tec.com" className="hover:text-white">alihammadg2003@gmail.com</a></p>
+            <p>Email: <Link href="mailto:contact@c2tec.com" className="hover:text-white">alihammadg2003@gmail.com</Link></p>
             <p>Phone: <span className="hover:text-white">0320-9673373</span></p>
           </div>
         </div>

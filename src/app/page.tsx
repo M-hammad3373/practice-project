a3ab24c7c0077d7b073a19f6cf0d3ce9663bceea
M-hammad3373 <alihammadg2003@gmail.com>
2024-10-27 00:59:06 +0500

    ufff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import Navbar from '@/components/Navbar'
+
 import HeroSection from '@/components/HeroSection'
 import FeaturedCourses from '@/components/FeaturedCourses'
 import WhyChooseUs from '@/components/WhyChooseUs'
 import TestiMonialCard from '@/components/TestiMonialCard'
-import Footer from '@/components/Footer'
+
 import Wabinars from '@/components/Wabinars'
 
 export const page = () => {
   return (
     <div>
-      <Navbar/>
+  
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
       <Wabinars/>
       <TestiMonialCard/>
-      <Footer/>
+      
 
     </div>
   )
